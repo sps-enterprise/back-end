dc4f43b615105ee8e345f0fd3caf5653d8812670
joao matheus <joao.duavy@ccc.ufcg.edu.br>
2022-12-07 16:48:53 -0300

    Add routes names

--- src/routes/empresas.js
+++ src/routes/empresas.js
@@ -5,9 +5,9 @@ const router = express.Router();
 const empresasController = require('../controllers/empresasController');
 const empresasMiddleware = require('../middlewares/empresasMiddleware');
 
-router.get('/', empresasController.getAll);
-router.post('/', empresasMiddleware.validateBody, empresasController.createEmpresa);
-router.delete('/:id', empresasController.deleteEmpresa);
-router.put('/:id', empresasMiddleware.validateBody, empresasController.updateEmpresa);
+router.get('/list', empresasController.getAll);
+router.post('/create', empresasMiddleware.validateBody, empresasController.createEmpresa);
+router.delete('/delete/:id', empresasController.deleteEmpresa);
+router.put('/delete/:id', empresasMiddleware.validateBody, empresasController.updateEmpresa);
 
 module.exports = router;
