6c2ae3fc81bc764969fc1052935ac35ab0088de8
Rembrandt <rembrandt-258@hotmail.com>
2022-12-10 21:35:49 -0300

    Add swagger docs

--- src/app.js
+++ src/app.js
@@ -4,7 +4,15 @@ const app = express();
 
 const router = require('./routes');
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
+const bodyParser = require('body-parser')
+
 app.use(express.json());
+
 app.use(router);
+
+app.use(bodyParser.json())
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 module.exports = app;
