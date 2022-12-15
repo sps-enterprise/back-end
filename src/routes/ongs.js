d50fbee2f9a70d77563a2716eac68f59033dc5e9
caio-galvao <caio.r.galvao@gmail.com>
2022-12-15 10:22:11 -0300

    Corrige rotas de ongs

--- src/routes/ongs.js
+++ src/routes/ongs.js
@@ -5,15 +5,15 @@ const router = express.Router();
 const ongsController = require("../controllers/ongsController");
 const ongsMiddleware = require("../middlewares/ongsMiddleware");
 
-router.get("/listOng", ongsController.getAll);
+router.get("/", ongsController.getAll);
 router.post(
-  "/createOng",
+  "/",
   ongsMiddleware.validateFieldTitle,
   ongsController.createONG
 );
-router.delete("/deleteOng:id", ongsController.deleteONG);
+router.delete("/:id", ongsController.deleteONG);
 router.put(
-  "/updateOng/:id",
+  "/:id",
   ongsMiddleware.validateFieldTitle,
   ongsMiddleware.validateFieldStatus,
   ongsController.updateONG
