62bd16b847f193654933bc30afca94e04985baa7
davihsg <daviandlion@gmail.com>
2022-12-15 23:00:52 -0300

    Fix database queries and add get ONG path

--- src/models/ongsModel.js
+++ src/models/ongsModel.js
@@ -1,31 +1,33 @@
 const db = require('./database');
 
 const getAll = async () => {
-  const [ongs] = await db.exec('SELECT * FROM ong');
+  const ongs = await db.exec('SELECT * FROM ong');
   return ongs.rows;
 };
+
+const getONG = async (cnpj) => {
+  const ong = await db.exec('SELECT * FROM ong WHERE cnpj = $1', [cnpj]);
+  return ong.rows;
+} 
 
 const createONG = async (ONG) => {
   const q = 'INSERT INTO ong(cnpj, nome, endereco, telefone, email) VALUES ($1, $2, $3, $4, $5)';
-  const [ongs] = await db.exec(q, Object.values(ONG));
-  return ongs.rows;
+  await db.exec(q, Object.values(ONG));
 };
 
 const deleteONG = async (cnpj) => {
-  const [ong] = await db.exec('DELETE FROM ong WHERE cnpj = $1', [cnpj]);
-  return ong.rows;
+  await db.exec('DELETE FROM ong WHERE cnpj = $1', [cnpj]);
 };
 
 const updateONG = async (cnpj, ONG) => {
   const q = 'UPDATE ong SET cnpj = $1, nome = $2, endereco = $3, telefone = $4, email = $5 WHERE cnpj = $6';
-  var v = Object.values(ONG)
-  v = [...v, cnpj];
-  const [ong] = await db.exec(q, v);
-  return ong.rows;
+  const v = [...Object.values(ONG), cnpj]
+  await db.exec(q, v);
 };
 
 module.exports = {
   getAll,
+  getONG,
   createONG,
   deleteONG,
   updateONG,
