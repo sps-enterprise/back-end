be4c371d03cf2affce8c41a40ba07964e4a3ed84
caio <caio.r.galvao@gmail.com>
2023-01-22 16:23:08 -0300

    Adiciona autenticação para posts e produto

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -4,17 +4,20 @@ const router = express.Router();
 
 const postsControlller = require('../controllers/postsController');
 const postsMiddleware = require('../middlewares/postsMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.get('/', postsControlller.getAll);
 router.get('/:id', postsControlller.getPost);
 router.post('/', 
+  authMiddleware, 
   postsMiddleware.validateBody, 
   postsMiddleware.validateCNPJ, 
   postsMiddleware.validateIdProduto, 
   postsControlller.createPost,
 );
-router.delete('/:id', postsControlller.deletePost);
+router.delete('/:id', authMiddleware, postsControlller.deletePost);
 router.put('/:id', 
+  authMiddleware,
   postsMiddleware.validateBody, 
   postsMiddleware.validateCNPJ, 
   postsMiddleware.validateIdProduto, 
--- src/routes/produto.js
+++ src/routes/produto.js
@@ -4,12 +4,13 @@ const router = express.Router();
 
 const produtoController = require('../controllers/produtoController.js');
 const produtoMiddleware = require('../middlewares/produtoMiddleware.js');
+const authMiddleware = require('../middlewares/authMiddleware');
              
 router.get('/', produtoController.getAll);
 router.get('/:id', produtoController.getProduto);
-router.post('/', produtoMiddleware.validateBody, produtoController.createProduto);
-router.delete('/:id', produtoController.deleteProduto);
-router.put('/:id', produtoMiddleware.validateBody, produtoController.updateProduto,
+router.post('/', authMiddleware, produtoMiddleware.validateBody, produtoController.createProduto);
+router.delete('/:id', authMiddleware, produtoController.deleteProduto);
+router.put('/:id', authMiddleware, produtoMiddleware.validateBody, produtoController.updateProduto,
 );
 
 module.exports = router;
