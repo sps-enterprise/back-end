0f428b55cc10377f9d0223dd0702c2f13968c299
davihsg <daviandlion@gmail.com>
2023-01-25 19:10:14 -0300

    fix: update database queries with correct syntax

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
-router.use("/ongs", require("./ongs"));
-router.use("/empresas/", require("./empresas"));
-router.use("/admin/", require("./admin"));
+router.use('/ongs', require('./ongs'));
+router.use('/empresas', require('./empresas'));
+router.use('/admin', require('./admin'));
 
 module.exports = router;
